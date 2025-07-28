899df6941895b83e657d7cdc6383e806ede0e9c6
Lahindu <lahindu2001ishan@gmail.com>
2025-07-28 20:36:02 +0530

    app js update

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log("lahindu");
+//pasword 5ujIqBeOwJYjq1hM
+
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+//midlware
+app.use("/",( req , res , next) => {
+    res.send("It is working");
+})
+
+
+mongoose.connect("mongodb+srv://admin:5ujIqBeOwJYjq1hM@cluster1.lmzaxue.mongodb.net/")
+.then(() => console.log("connected to MongoDB"))
+.then(() =>  {
+    app.listen(5000);
+})
+.catch((err) => console.log((err)));
