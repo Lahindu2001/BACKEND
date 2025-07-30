2606889ed101c9c8e1fc0fb67909cf0e7fa2159b
Lahindu <lahindu2001ishan@gmail.com>
2025-07-30 20:29:07 +0530

    delete parts done

--- Controlers/UserControlers.js
+++ Controlers/UserControlers.js
@@ -137,6 +137,42 @@ const updateUser = async (req, res, next) => {
 
 
 
+//delete parts
+
+const deleteUser = async (req, res, next) => {
+    const id = req.params.id;
+
+    let user;
+    try{
+        user = await User.findByIdAndDelete(id)
+    }catch(err){
+        console.log(err)
+    }
+
+
+    if (!user) {
+        return res.status(404).json({ message: "Unable to selete user detail" });
+    }
+
+    return res.status(200).json({ user });
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -145,3 +181,4 @@ exports.getAllUsers = getAllUsers;
 exports.addUsers = addUsers;
 exports.getbyId = getbyId ;
 exports.updateUser = updateUser ;
+exports.deleteUser = deleteUser ;
